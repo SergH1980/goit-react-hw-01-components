846c61085f02a909edd97860b25ff6cbf5c8b413
Serhii Halaka <serg.galaka@gmail.com>
2023-04-11 19:01:15 +0300

    task 2 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile/Profile';
 import user from './JSON/user.json';
+import { Statistics } from './Statistics/Statistics';
+import statistics from './JSON/data.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statistics} />
     </div>
   );
 };
